9a2e04980a819a78353d5631bdfe170ac9b2adaf
vmathur <vmathur@users.noreply.github.com>
2021-05-18 22:27:22 -0700

    create collectible and add to set test

--- test/set.test.js
+++ test/set.test.js
@@ -9,6 +9,9 @@ beforeAll(() => {
   init(basePath);
 });
 
+let setId = ''
+let collectibleId = ''
+
 test("Create Set", async () => {
     const addressMap = {KOTD: config["0xAdmin"]};
     const create_set = await getTransactionCode({name: "admin/create_set", addressMap}) 
@@ -18,6 +21,7 @@ test("Create Set", async () => {
     try {
         const txResult = await sendTransaction({ code: create_set, args, signers });
         console.log({ txResult });
+        setId = txResult.events[0].data.setID
     } catch (e) {
         console.log(e);
     }
@@ -26,7 +30,7 @@ test("Create Set", async () => {
 test("Get Set", async () => {
     const addressMap = {KOTD: config["0xAdmin"]};
     const get_set_name = await getScriptCode({name: "get_set_name", addressMap})
-    const args = [[1, UInt32]]
+    const args = [[setId, UInt32]]
 
     try {
         const res = await executeScript({ code: get_set_name, args });
@@ -40,10 +44,54 @@ test("Create collectible item", async () => {
     const addressMap = {KOTD: config["0xAdmin"]};
     const create_collectibe_item = await getTransactionCode({name: "admin/create_collectible_item", addressMap}) 
     const signers = [config["0xAdmin"]]
-    const args = [['New collectibe', FlowString]]
+    const args = [['New collectible', FlowString]]
 
     try {
         const res = await sendTransaction({ code: create_collectibe_item, args, signers });
+        console.log({ res });
+        collectibleId = res.events[0].data.id
+        console.log(res.events[0].data.metadata)
+    } catch (e) {
+        console.log(e);
+    }
+});
+
+test("Get collectible item meta data", async () => {
+    const addressMap = {KOTD: config["0xAdmin"]};
+    const get_collectible_item_metadata = await getScriptCode({name: "get_collectible_item_metadata", addressMap})
+    const signers = [config["0xAdmin"]]
+    const args = [[collectibleId, UInt32]]
+
+    try {
+        const res = await executeScript({ code: get_collectible_item_metadata, args, signers });
+        console.log({ res });
+    } catch (e) {
+        console.log(e);
+    }
+});
+
+test("Add collectible to set", async () => {
+    const addressMap = {KOTD: config["0xAdmin"]};
+    const add_collectible_to_set = await getTransactionCode({name: "admin/add_collectible_item_to_set", addressMap}) 
+    const signers = [config["0xAdmin"]]
+    const args = [[setId, UInt32], [collectibleId, UInt32]]
+
+    try {
+        const res = await sendTransaction({ code: add_collectible_to_set, args, signers });
+        console.log({ res });
+    } catch (e) {
+        console.log(e);
+    }
+});
+
+test("Get collectible items in a set", async () => {
+    const addressMap = {KOTD: config["0xAdmin"]};
+    const get_collectible_items_in_set = await getScriptCode({name: "get_collectible_items_in_set", addressMap}) 
+    const signers = [config["0xAdmin"]]
+    const args = [[setId, UInt32]]
+
+    try {
+        const res = await executeScript({ code: get_collectible_items_in_set, args, signers });
         console.log({ res });
     } catch (e) {
         console.log(e);
